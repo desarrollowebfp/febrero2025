e20036e4d38563db7e28790322813edf4ce61d48
Antonio Rosales Martínez <antoniorosalesmartinez90@gmail.com>
2025-06-02 20:56:38 +0200

    logic

---
+++ JAVASCRIPT/02_06_2025/1.operadoresAritmeticos.js
@@ -0,0 +1,6 @@
+let x = 76;
+x += 5;
+x -= 1;
+x++;
+x--;
+console.log(x);
---
+++ JAVASCRIPT/02_06_2025/2.operadoresLogicos.js
@@ -0,0 +1,19 @@
+// ->        || -> O -> OR
+// ->        && -> Y -> AND
+// ->        !  -> NO -> NOT
+// ->        ==  -> IGUAL QUE
+// ->        === -> ESTRICTAMENTE IGUAL QUE
+// ->        != -> DESIGUAL
+// ->        !== -> ESTRICTAMENTE DESIGUAL
+// ->        =  -> OPERADOR DE ASIGNACION
+// ->        < -> MENOR QUE
+// ->        <= -> MENOR O IGUAL QUE
+// ->        >  -> MAYOR QUE
+// ->        >=  -> MAYOR O IGUAL QUE
+
+const limpiarCuarto = true;
+const limpiarCocina = false;
+
+//console.log(limpiarCuarto && !limpiarCocina);
+
+console.log(25 == "25");
