d117dc67f49e497f51e0c01fffa5bc21945c24a4
Antonio Rosales Martínez <antoniorosalesmartinez@MacBook-Pro-de-Antonio.local>
2025-07-21 20:55:54 +0200

    feat vite

---
+++ JAVASCRIPT/VITE/21_07_2025/src/data.js
@@ -0,0 +1,27 @@
+const movies = [
+  {
+    title: "Matrix",
+    year: 1999,
+  },
+  {
+    title: "Matrix 2",
+    year: 2003,
+  },
+];
+
+const shows = [
+  {
+    title: "Tokyo Vice",
+    season: 1,
+  },
+  {
+    title: "Cyberpunk 2077",
+    season: 1,
+  },
+];
+
+const hello = () => {
+  console.log("Hello");
+};
+
+export { movies, shows, hello };
---
+++ JAVASCRIPT/VITE/21_07_2025/src/main.js
@@ -0,0 +1,8 @@
+import "./style.css";
+import { movies as peliculas, shows as series, hello as hola } from "./data.js";
+
+/* console.log(movies);
+console.log(shows);
+hello(); */
+console.log(peliculas);
+hola();
