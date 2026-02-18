8a99c55cbdfe7190477bca4dd9262de3ff06daa9
Antonio Rosales Martínez <antoniorosalesmartinez90@gmail.com>
2026-02-18 21:01:19 +0100

    feat backend

--- NOSQL/16_02_2026/src/controllers/videogames.controllers.js
+++ NOSQL/16_02_2026/src/controllers/videogames.controllers.js
@@ -4,7 +4,10 @@ const Videogame = require("../models/videogame.model");
 const getAllVideogames = async (req, res) => {
   try {
     const videogames = await Videogame.find();
-    return res.status(200).json(videogames);
+    return res.status(200).json({
+      total_videogames: await Videogame.countDocuments(),
+      results: videogames
+    });
   } catch (error) {
     return res.status(500).json(error);
   }
@@ -27,4 +30,66 @@ const getVideogameByID = async (req, res) => {
   }
 };
 
-module.exports = { getAllVideogames, getVideogameByID };
+const getVideogamesByPlatform = async (req, res) => {
+  const { platform } = req.params;
+  try {
+    const videogames = await Videogame.find({ platform: platform });
+    return res.status(200).json(videogames);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+
+const createVideogame = async (req, res) => {
+  try {
+    const newVideogame = new Videogame(req.body);
+    const createdVideogame = await newVideogame.save();
+    return res.status(201).json(createdVideogame);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+
+const deleteVideogame = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const videogame = await Videogame.findByIdAndDelete(id);
+    if (!videogame) {
+      return res.status(404).json("Videojuego a borrar no encontrado");
+    }
+    return res.status(200).json({
+      message: `Videojuego ${videogame.title} borrado`,
+    });
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+
+const updateVideogame = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const updated = await Videogame.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!updated) {
+      return res
+        .status(404)
+        .json("El videojuego a actualizar no ha sido encontrado");
+    }
+
+    return res.status(200).json(updated);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+
+module.exports = {
+  getAllVideogames,
+  getVideogameByID,
+  getVideogamesByPlatform,
+  createVideogame,
+  deleteVideogame,
+  updateVideogame
+};
--- NOSQL/16_02_2026/src/routes/videogames.routes.js
+++ NOSQL/16_02_2026/src/routes/videogames.routes.js
@@ -1,11 +1,22 @@
 const express = require("express");
 
-const { getAllVideogames, getVideogameByID } = require("../controllers/videogames.controllers");
+const {
+  getAllVideogames,
+  getVideogameByID,
+  getVideogamesByPlatform,
+  createVideogame,
+  deleteVideogame,
+  updateVideogame,
+} = require("../controllers/videogames.controllers");
 
 const router = express.Router();
 
 // Especificamos las rutas que disparan los controladores
 router.get("/", getAllVideogames);
 router.get("/:id", getVideogameByID);
+router.get("/platform/:platform", getVideogamesByPlatform);
+router.post("/", createVideogame);
+router.delete("/:id", deleteVideogame);
+router.put("/:id", updateVideogame);
 
-module.exports = router
+module.exports = router;
